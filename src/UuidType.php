31c6b0f24a1cc050cd3c2bbdd0954dad8902e1e6
Ben Ramsey <ben@benramsey.com>
2015-09-28 16:14:36 -0500

    Update file-level docblock headers

--- src/UuidType.php
+++ src/UuidType.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * This file is part of the Ramsey\Uuid\Doctrine library
+ * This file is part of the ramsey/uuid-doctrine library
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
